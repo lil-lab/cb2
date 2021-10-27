2dfd3b4fea0cd8c35ad3d52a7e4bbbdb95616acc
Jacob Sharf <jacobsharf@gmail.com>
2021-10-27 17:58:44 -0400

    Fix to deploy script.

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,4 +1,4 @@
 UNITY="/Applications/Unity/Hub/Editor/2020.3.19f1/Unity.app/Contents/MacOS/Unity"
 $UNITY -quit -batchmode -logFile - -projectPath game/ -executeMethod WebGLBuilder.Build
-cp -r game/builds/WebGLVersion server/www/WebGL
+cp -rf game/builds/WebGLVersion server/www/WebGL
 echo "Moved build to server/www directory"
