5fd5c8e8d96e2617574aec488595411c96768879
Jacob Sharf <jacobsharf@gmail.com>
2021-10-29 17:02:22 -0400

    Slight change to build script to *ensure* that new builds are deployed to the server.

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,4 +1,6 @@
 UNITY="/Applications/Unity/Hub/Editor/2020.3.19f1/Unity.app/Contents/MacOS/Unity"
 $UNITY -quit -batchmode -logFile - -projectPath game/ -executeMethod WebGLBuilder.Build
+rm -rf server/www/OLD_WebGL
+mv server/www/WebGL server/www/OLD_WebGL
 cp -rf game/builds/WebGLVersion server/www/WebGL
 echo "Moved build to server/www directory"
