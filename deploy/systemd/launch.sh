036215276b3f19bfc2f3ac277c8605ad698dfccb
Jacob Sharf <jacobsharf@gmail.com>
2022-01-27 06:04:22 +0000

    Created deploy script for installing the server on a new computer.

--- deploy/systemd/launch.sh
+++ deploy/systemd/launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cb2_install_location="/home/lil-lab/projects/cb2-game-dev"
+cb2_install_location="/home/ubuntu/projects/cb2-game-dev"
 
 cd $cb2_install_location
 source server/venv/bin/activate
