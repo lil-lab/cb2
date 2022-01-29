f575129c05a42c0dc47a62253e06989d76fc6827
Jacob Sharf <jacobsharf@gmail.com>
2022-01-28 19:24:19 -0500

    Adds pip dependency to deploy script.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,7 @@
 sudo apt install python3.8-venv
 sudo apt install gcc
 sudo apt install sqlite3 libsqlite3-dev
-sudo apt install python-dev cython
+sudo apt install python-dev cython python3-pip
 sudo apt-get install python3-dev
 cd ../server/
 sudo -S -u ubuntu -E env PATH=$PATH python3 -m venv --system-site-packages venv
