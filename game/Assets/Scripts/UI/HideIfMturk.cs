a1f34b186a2c4f9ae4cd6d01efa10250d7aa9b38
Jacob Sharf <jacobsharf@gmail.com>
2022-02-01 19:18:50 -0500

    Improves tutorial flow to prevent mturk workers from going to main menu.

---
+++ game/Assets/Scripts/UI/HideIfMturk.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideIfMturk : MonoBehaviour
+{
+    void Start()
+    {
+        // Only applies to WebGL contexts...
+        if (Application.platform != RuntimePlatform.WebGLPlayer)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+        Dictionary<string, string> urlParameters = Network.NetworkManager.UrlParameters();
+        gameObject.SetActive(!urlParameters.ContainsKey("assignmentId"));
+    }
+}
