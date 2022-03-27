6ee41c2ad5518c86dddd84ce994e0586e47bb51c
Jacob Sharf <jacobsharf@gmail.com>
2022-03-27 02:55:39 -0400

    Updated deploy script with new dependency.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,7 @@ sudo apt install python3.8-venv
 sudo apt install gcc
 sudo apt install sqlite3 libsqlite3-dev
 sudo apt install python-dev cython python3-pip
+sudo apt install cargo
 sudo apt-get install python3-dev
 cd ../server/
 sudo -S -u ubuntu -E env PATH=$PATH python3 -m venv --system-site-packages venv
