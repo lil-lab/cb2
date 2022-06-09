1df1b84d8976d0f7b79585e949d855f86d804c2e
Jacob Sharf <jacobsharf@gmail.com>
2022-06-08 21:27:20 -0400

    Fixes card cover rendering code.

--- game/Assets/Scripts/UI/ShowCardCovers.cs
+++ game/Assets/Scripts/UI/ShowCardCovers.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class ShowCardCovers : MonoBehaviour
 {
+    private bool _cardCoversSet = false;
+    private datetime _lastTry = datetime.MinValue;
+    private Logger _logger;
     // Start is called before the first frame update
     void Start()
     {
+        _logger = Logger.GetOrCreateTrackedLogger("ShowCardCovers");
+    }
+
+    public void Update()
+    {
+        if (_cardCoversSet) return;
+        // Try this every 3 seconds.
+        if (datetime.Now - _lastTry < TimeSpan.FromSeconds(3)) return;
+
         Network.NetworkManager networkManager = Network.NetworkManager.TaggedInstance();
+        if (networkManager.ServerConfig() == null) return;
+
         Camera camera = GetComponent<Camera>();
-        if (networkManager.ServerConfig() != null && networkManager.ServerConfig().card_covers)
+        if (networkManager.ServerConfig().card_covers)
         {
             camera.cullingMask = camera.cullingMask | (1 << LayerMask.NameToLayer("card_covers"));
         } else {
             camera.cullingMask = camera.cullingMask & ~(1 << LayerMask.NameToLayer("card_covers"));
         }
+        _logger.Info("Set card covers to " + networkManager.ServerConfig().card_covers);
+        _cardCoversSet = true;
     }
 }
