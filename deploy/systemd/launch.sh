fa6075374eab020da2b48ebbf536b973a47a5d21
Jacob Sharf <jacobsharf@gmail.com>
2022-08-25 15:47:07 -0400

    Update systemd daemon with new paths.

--- deploy/systemd/launch.sh
+++ deploy/systemd/launch.sh
@@ -3,6 +3,5 @@
 cb2_install_location="/home/ubuntu/projects/cb2-game-dev"
 
 cd $cb2_install_location
-source server/venv/bin/activate
-cd server
-python3 -m main
+source venv/bin/activate
+python3 -m server.main
