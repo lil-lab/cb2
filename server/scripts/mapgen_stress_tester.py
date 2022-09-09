d5ab64d3a5bb295a77ca861bf88ce048e94e6b35
Jacob Sharf <jacobsharf@gmail.com>
2022-09-09 11:23:26 -0400

    Deletes old tool. Adds mapgen stress tester.

---
+++ server/scripts/mapgen_stress_tester.py
@@ -0,0 +1,17 @@
+import fire
+import time
+from map_provider import MapProvider, MapType
+
+def main(number_maps = 100):
+    # Generate number_maps number of random maps.
+    start_time = time.time()
+    for i in range(number_maps):
+        map_provider = MapProvider(MapType.RANDOM)
+        map = map_provider.map()
+        print(f"Generated map {i}: map: {map}")
+    end_time = time.time()
+    print(f"Generated {number_maps} maps in {end_time - start_time} seconds.")
+    print(f"Time per map: {(end_time - start_time) / number_maps}")
+
+if __name__ == "__main__":
+    fire.Fire(main)
