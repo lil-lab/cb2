ac1822a38cf9731f6eab78ebd2556c9cf4d62279
Jacob Sharf <jacobsharf@gmail.com>
2022-11-26 02:04:49 -0800

    Adds follower pilot to initialization routine.

--- server/lobby_utils.py
+++ server/lobby_utils.py
@@ -1,10 +1,14 @@
 """A set of methods for registering and accessing lobbies"""
+import logging
 from typing import List
 
+from server.follower_pilot_lobby import FollowerPilotLobby
 from server.google_lobby import GoogleLobby
 from server.lobby_consts import LobbyInfo, LobbyType
 from server.mturk_lobby import MturkLobby
 from server.open_lobby import OpenLobby
+
+logger = logging.getLogger(__name__)
 
 # Map from lobby name to lobby.
 lobbies = {}
@@ -32,5 +36,7 @@ def CreateLobby(lobby_name: str, lobby_type: LobbyType):
         lobbies[lobby_name] = GoogleLobby(lobby_name)
     elif lobby_type == LobbyType.OPEN:
         lobbies[lobby_name] = OpenLobby(lobby_name)
+    elif lobby_type == LobbyType.FOLLOWER_PILOT:
+        lobbies[lobby_name] = FollowerPilotLobby(lobby_name)
     else:
         raise ValueError(f"Invalid lobby type: {lobby_type}")
