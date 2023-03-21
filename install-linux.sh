ade49defc1dfc54857e7de35975c5926a5202108
Jacob Sharf <jacobsharf@gmail.com>
2023-03-21 09:35:55 -0400

    Install script update.

--- install-linux.sh
+++ install-linux.sh
@@ -1,17 +1,17 @@
-set -x
+set -ex
 username=$(whoami)
 echo "Installing some system packages: python3.9-venv, gcc, sqlite3, cython, cargo."
-sudo apt install python3.9-venv gcc sqlite3 libsqlite3-dev python-dev cython python3-pip cargo
+sudo apt install python3 gcc sqlite3 libsqlite3-dev cython cargo unzip
 echo "Setting up python env..."
 echo "Your python version is: "
-python3.9 --version
+python3 --version
 echo "If it is not 3.9+, you may experience issues running CB2."
 echo "Additionally, there may be issues running 3.11+."
-python3.9 -m venv venv
+python3 -m venv venv
 . ./venv/bin/activate
-python3.9 -m pip install wheel
-python3.9 -m pip install cython
-python3.9 -m pip install -r requirements.txt
+python3 -m pip install wheel
+python3 -m pip install cython
+python3 -m pip install -r requirements.txt
 echo "Downloading client..."
 cd server/www/
 wget https://github.com/lil-lab/cb2/releases/download/deployed-march-2023/WebGL.zip
@@ -20,5 +20,5 @@ unzip WebGL
 cd -
 echo "Running local self-play as a test..."
 sleep 1
-python3.9 -m py_client.demos.local_self_play --num_games=10 --config_filepath="server/config/local-covers-config.yaml"
+python3 -m py_client.demos.local_self_play --num_games=10 --config_filepath="server/config/local-covers-config.yaml"
 echo "DONE"
